94f5636e4b4edf22c78db16194cd7770dbb3929e
Ramiz Khan <ramizuddinboss@gmail.com>
2025-07-16 22:46:33 +0530

    feat: update package.json for homepage, build, and lint scripts
    chore: replace ramiz.jpg with updated version
    
    feat: add vite-env.d.ts for Vite client types
    
    feat: create tsconfig.app.json for application TypeScript configuration
    
    chore: add tsconfig.json for project references
    
    feat: create tsconfig.node.json for Node-specific TypeScript configuration
    
    feat: enhance vite.config.js with build and server configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ramizkhan/',
+
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
